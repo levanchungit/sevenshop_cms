7636b8bae51ae51404d515c764a0715746eddecd
levanchung.it <levanchung.it@gmail.com>
2023-03-24 14:14:41 +0700

    fix host

--- src/global/config/index.ts
+++ src/global/config/index.ts
@@ -1,5 +1,5 @@
-const isProduction = process.env.NODE_ENV === 'production'
+export const API_URL = 'https://sevenshop.herokuapp.com'
 
-export const API_URL = isProduction ? process.env.HOST : process.env.LOCAL
+// export const API_URL = 'http://localhost:3000'
 
 console.info('API_URL => ', API_URL)
